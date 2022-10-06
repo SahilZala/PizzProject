5cecd1bb59db9851932a9f2387e8ef6d14cb4ed6
sahilz <zala4011@gmail.com>
2022-10-06 11:47:34 +0530

    change: logic change in getCustomerOrderDataController.

--- src/main/java/com/pack/controller/GetCustomerOrderDataController.java
+++ src/main/java/com/pack/controller/GetCustomerOrderDataController.java
@@ -4,9 +4,12 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.pack.model.OrderModel;
 import com.pack.model.StatusModel;
+import com.pack.model.UserModel;
 import com.pack.service.OrderModelService;
 import com.pack.service.UserModelService;
 import com.pack.util.PathsClass;
 import com.pack.util.UserCredentials;
 
 @RestController
+@CrossOrigin
 @RequestMapping(PathsClass.CUSTOMER_PATH)
 public class GetCustomerOrderDataController {
 	
@@ -29,16 +34,20 @@ public class GetCustomerOrderDataController {
 	@Autowired
 	UserModelService userModelService;
 	
+	@Autowired
+	UserCredentials userCredentials;
+	
 	
 	@GetMapping(PathsClass.GET_CUSTOMER_ORDER_DATA_PATH)
-	@CrossOrigin(origins = "*")
-	public ResponseEntity<StatusModel> getCustomerOrderDataController(){
+	public ResponseEntity<StatusModel> getCustomerOrderDataController(HttpServletRequest request){
+		
+		UserDetails ud = userCredentials.getUserDetails(request.getHeader("Authorization"));
 		
 		List<OrderModel> orderModelList = new ArrayList<>();
 		try {
+			
 			orderModelList = orderModelService.getAllOrdersByUserId(
-					userModelService.getUserData(UserCredentials.getUserDetails().getUsername(),
-						UserCredentials.getUserDetails().getPassword())
+					userModelService.getUserData(ud.getUsername(),ud.getPassword())
 			);
 			
 			return new ResponseEntity<>(
@@ -58,6 +67,5 @@ public class GetCustomerOrderDataController {
 							PathsClass.CUSTOMER_PATH+PathsClass.GET_CUSTOMER_ORDER_DATA_PATH,
 							"no data"),HttpStatus.BAD_REQUEST);
 		}
-		
 	}
 }
